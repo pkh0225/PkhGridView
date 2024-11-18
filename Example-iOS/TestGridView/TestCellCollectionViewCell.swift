218a27fdf06efd08057cb195a5883130ae4e5922
pkh0225 <pkh0225@ssg.com>
2024-11-18 13:07:24 +0900

    .
    .

--- Example-iOS/TestGridView/TestCellCollectionViewCell.swift
+++ Example-iOS/TestGridView/TestCellCollectionViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PkhGridView
 
 class TestCellCollectionViewCell: UICollectionViewCell, PkhGridViewProtocol {
     var actionClosure: ((_ name: String, _ object: Any?) -> Void)?
--- Example-iOS/TestGridView/TestCellCollectionViewCell2.swift
+++ Example-iOS/TestGridView/TestCellCollectionViewCell2.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PkhGridView
 
 class TestCellCollectionViewCell2: UICollectionViewCell, PkhGridViewProtocol {
     var actionClosure: ((_ name: String, _ object: Any?) -> Void)?
